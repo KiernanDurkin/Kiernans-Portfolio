3bdbf4be5dd71a632f24a1c797300829cb21161d
KiernanDurkin <32209862+KiernanDurkin@users.noreply.github.com>
2018-04-16 19:03:44 -0400

    sweet alert

---
+++ js/sweetAlert.js
@@ -0,0 +1,15 @@
+
+
+function validation(){
+
+
+    swal({
+        title: "Message Sent!",
+        text: "I'll get back to you ASAP",
+        icon: "success",
+        button: "Aww yiss!",
+    });
+    
+   
+    document.getElementById('myContactForm').reset(); 
+}
